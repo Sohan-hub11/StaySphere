b569f682af9391710269426263f61a6230b664a4
Sohan Samanta <sohansamanta18@gmail.com>
2026-01-27 03:46:30 +0530

    v3

--- models/listing.js
+++ models/listing.js
@@ -17,10 +17,6 @@ const listingSchema = new Schema({
         ? "https://images.unsplash.com/photo-1610147323479-a7fb11ffd5dd?w=600&auto=format&fit=crop&q=60"
         : v
   },
-  filename: {
-    type: String,
-    default: "listingimage"
-  }
 },
 
   price: Number,
