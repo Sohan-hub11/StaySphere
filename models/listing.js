3060f1cae475a3e87c1d2a8fd3c90ca64275081f
Sohan Samanta <sohansamanta18@gmail.com>
2026-01-26 22:19:48 +0530

    v3

--- models/listing.js
+++ models/listing.js
@@ -8,12 +8,21 @@ const listingSchema = new Schema({
   },
   description: String,
   image: {
+  url: {
     type: String,
-    default: "https://images.unsplash.com/photo-1610147323479-a7fb11ffd5dd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D",
-    set: (v) => v === "" 
-      ? "https://images.unsplash.com/photo-1610147323479-a7fb11ffd5dd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D" 
-      : v,
+    default:
+      "https://images.unsplash.com/photo-1610147323479-a7fb11ffd5dd?w=600&auto=format&fit=crop&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1610147323479-a7fb11ffd5dd?w=600&auto=format&fit=crop&q=60"
+        : v
   },
+  filename: {
+    type: String,
+    default: "listingimage"
+  }
+},
+
   price: Number,
   location: String,
   country: String
